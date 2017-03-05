d3a39fd753c4bdd7e06e90f0fdd2c36b426c3291
arraqib <raqibjs93@gmail.com>
2017-03-06 00:52:55 +0600

    Improve detection condition

--- main.m
+++ main.m
@@ -7,9 +7,8 @@ close all;
 % Read mean from training images
 [redMean, greenMean, blueMean] = meansOfFlood();
 
-tic;
-
-img = imread('test1.jpg');
+%% Read image
+img = imread('test6.jpg');
 [row col dim] = size(img);
 im = double(img);
 
@@ -23,7 +22,7 @@ for x=1:1:row
         redVal = abs(red(x,y) - redMean);
         greenVal = abs(green(x,y) - greenMean);
         blueVal = abs(blue(x,y) - blueMean);
-        sd = 50;
+        sd = 48;
         if((redVal <= sd) && (greenVal <= sd) && (blueVal <= sd))
             biIm(x,y) = 1;
         else
@@ -33,8 +32,6 @@ for x=1:1:row
 end
 
 %% Remove noise effect and narrow connection
-numberOfPixels = numel(biIm);
-
 sedisk = strel('disk',2);
 openedIm = imopen(biIm, sedisk);
 
@@ -42,36 +39,41 @@ openedIm = imopen(biIm, sedisk);
 closedIm = imclose(openedIm, sedisk);
 
 %% Delete small objects
-removeTh = round(numberOfPixels - numberOfPixels * 92 / 100); % remove when pixel number 8% less than total pixel; 
+numberOfPixels = numel(biIm);
+removeTh = round(numberOfPixels - numberOfPixels * 90 / 100); % remove when pixel number 8% less than total pixel; 
 filteredIm = bwareaopen(closedIm, removeTh);
 
-%% Give warning if water logged area found
+%% If water logged area found
 numberOfTruePixels = sum(filteredIm(:));
+
 if(numberOfTruePixels > 0)
     disp('Warning: Water logged area! ');
-else
-    disp('No Water');
-end
 
-toc;
-
-%% Making output image
-for x=1:1:row
-    for y=1:1:col
-        if(filteredIm(x,y) > 0)
-            red(x,y) = 255;
-            green(x,y) = 0;
-            blue(x,y) = 0;
-        else
-            red(x,y) = red(x,y);
-            green(x,y) = green(x,y);
-            blue(x,y) = blue(x,y);
+    % Making output image if it contain water
+    for x=1:1:row
+        for y=1:1:col
+            if(filteredIm(x,y) > 0)
+                red(x,y) = 255;
+                green(x,y) = 0;
+                blue(x,y) = 0;
+            else
+                red(x,y) = red(x,y);
+                green(x,y) = green(x,y);
+                blue(x,y) = blue(x,y);
+            end
         end
     end
-end
 
-%% Make RGB image from individual R, G, B plane
-newIm = cat(3, red, green, blue);
+
+    % Make RGB image from individual R, G, B plane
+    newIm = cat(3, red, green, blue);
+
+%% No water logged area
+else
+    disp('No Water');
+    newIm = img;
+
+end
 
 subplot(1,2,1);
 imshow(uint8(img));
--- waterDetection.m
+++ waterDetection.m
@@ -3,12 +3,14 @@ clear all;
 close all;
 
 %% Calculate mean from traing water images
+
 [redMean, greenMean, blueMean] = meansOfFlood();
 
 %% Get video sequences
-videoObj = VideoReader('no-water2.mp4');
+videoObj = VideoReader('water3.mp4');
 
 %% Get all frame from the video object
+
 nframes = get(videoObj, 'NumberOfFrames');
 
 %% Take one frame to make a result container
@@ -23,18 +25,18 @@ for k = 1 : nframes
     img = read(videoObj, k);
     [row col dim] = size(img);
     im = double(img);
-
+    
     red = im(:, :, 1);
     green = im(:, :, 2);
     blue = im(:, :, 3);
-
+    
     %% Color analysis
     for x=1:1:row
         for y=1:1:col
             redVal = abs(red(x,y) - redMean);
             greenVal = abs(green(x,y) - greenMean);
             blueVal = abs(blue(x,y) - blueMean);
-            sd = 50;
+            sd = 48;
             if((redVal <= sd) && (greenVal <= sd) && (blueVal <= sd))
                 biIm(x,y) = 1;
             else
@@ -44,48 +46,52 @@ for k = 1 : nframes
     end
 
     %% Remove noise effect and narrow connection
-    numberOfPixels = numel(biIm);
-
     sedisk = strel('disk',2);
     openedIm = imopen(biIm, sedisk);
 
     %% Filling small holes inside detected region
     closedIm = imclose(openedIm, sedisk);
-
+    
     %% Delete small objects
-    removeTh = round(numberOfPixels - numberOfPixels * 92 / 100); % remove when pixel number 8% less than total pixel; 
+
+    numberOfPixels = numel(biIm);
+    removeTh = round(numberOfPixels - numberOfPixels * 90 / 100); % remove when pixel number 8% less than total pixel; 
     filteredIm = bwareaopen(closedIm, removeTh);
 
-    %% Give warning if water logged area found
+    %% If water logged area found
     numberOfTruePixels = sum(filteredIm(:));
+    
     if(numberOfTruePixels > 0)
         disp('Warning: Water logged area! ');
-    else
-        disp('No Water');
-    end
-
-    toc;
-
-    %% Making output image
-    for x=1:1:row
-        for y=1:1:col
-            if(filteredIm(x,y) > 0)
-                red(x,y) = 255;
-                green(x,y) = 0;
-                blue(x,y) = 0;
-            else
-                red(x,y) = red(x,y);
-                green(x,y) = green(x,y);
-                blue(x,y) = blue(x,y);
+        % Making output image if it marking water region
+        for x=1:1:row
+            for y=1:1:col
+                if(filteredIm(x,y) > 0)
+                    red(x,y) = 255;
+                    green(x,y) = 0;
+                    blue(x,y) = 0;
+                else
+                    red(x,y) = red(x,y);
+                    green(x,y) = green(x,y);
+                    blue(x,y) = blue(x,y);
+                end
             end
         end
-    end
+        
 
-    %% Make RGB image from individual R, G, B plane
-    newIm = cat(3, red, green, blue);
-    
-    %% Store processed image into result container
-    results(:,:,:,k) = newIm;
+        % Make RGB image from individual R, G, B plane
+        newIm = cat(3, red, green, blue);
+        
+        % Store processed image into result container
+        results(:,:,:,k) = newIm;
+
+    %% No water logged area
+    else
+        disp('No Water');
+        % Store input image into result container
+        results(:,:,:,k) = img;
+    end
+    toc;
 end
 
 frameRate = get(videoObj,'FrameRate');
